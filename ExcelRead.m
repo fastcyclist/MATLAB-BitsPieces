54103a6cf051546b8a94810e3690e951c017f4a2
fastcyclist <jaelim2580@gmail.com>
2017-01-13 19:47:39 -0500

    Add files via upload

--- ExcelRead.m
+++ ExcelRead.m
@@ -1,7 +1,29 @@
-function [ output_args ] = ExcelRead( input_args )
-%EXCELREAD Summary of this function goes here
-%   Detailed explanation goes here
+function [Header, Number] = ExcelRead(PathName, FileName)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%       "xlsread" is commonly used to import Excel files in MATLAB. When
+%   a data file has non-numeric columns either at the beginning or at the 
+%   end, "xlsread" includes those columns in the header instead of numeric
+%   values. This leads to mismatch between the header cell array and the
+%   numbers array.
+%       "ExcelRead" uses the raw values and convert it to a correctly
+%   matched header and a numbers array.
 
+%% Read in the file as usual, using xlsread.
+[NUM,TXT,RAW]=xlsread([PathName, FileName]);
 
+if size(NUM,2) ~= size(TXT,2)   % if the numbers array and the header have different numbers of columns...
+    NRow_NUM = size(NUM,1);
+    NRow_RAW = size(RAW,1);
+    
+    Header      = RAW(1:(NRow_RAW-NRow_NUM),:);
+    Number_Temp = RAW((NRow_RAW-NRow_NUM)+1:end,:);
+    
+    Number      = zeros(size(Number_Temp));
+    for i=1:size(Number_Temp,1)
+        for j=1:size(Number_Temp,2)
+            if isnumeric(Number_Temp{i,j})
+                Number(i,j) = Number_Temp{i,j};
+            end
+        end
+    end
 end
-
